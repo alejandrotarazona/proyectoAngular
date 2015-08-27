e86ed70781d48058d72b2659ed8ab8197f80d554
Alejandro Tarazona <alejandrotarazona@gmail.com>
2015-08-27 17:27:46 -0430

    Mejorando userList

--- app/user/userListController.js
+++ app/user/userListController.js
@@ -1,5 +1,9 @@
 (function(){
 	hxplus.controller('ListUserController', function($state,$http,UserRepository){
 		this.listUsers = UserRepository.user.query();
+
+		editUser = function(user){
+			$state.go('home.userDetails',{id: user.id});
+		};
 	});
 })()
