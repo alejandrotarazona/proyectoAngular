5b7a998641560eaffa732a72c514d88babbc2e14
Alejandro Tarazona <alejandrotarazona@gmail.com>
2015-09-17 17:07:48 -0430

    Refinado el controlador de paciente

--- app/patient/newPatientController.js
+++ app/patient/newPatientController.js
@@ -2,7 +2,7 @@
 	hxplus.controller('NewPatientController', function($stateParams, $state, 
 			$translate,$http, UserRepository,PatientRepository,DoctorRepository){
 			this.users = UserRepository.usersPatients.query();
-
+			
 			var historyRequest = {};
 
 			historyRequest.allergies = [];
