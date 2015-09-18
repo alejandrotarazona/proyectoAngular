c8a059d97f4f4a00136e2fd69c916daba01275f1
Alejandro Tarazona <alejandrotarazona@gmail.com>
2015-09-18 17:13:58 -0430

    Agregar alergias dinámicas

--- app/patient/newPatientController.js
+++ app/patient/newPatientController.js
@@ -1,31 +1,26 @@
 (function(){
 	hxplus.controller('NewPatientController', function($stateParams, $state, 
 			$translate,$http, UserRepository,PatientRepository,DoctorRepository){
+
 			this.users = UserRepository.usersPatients.query();
+
+			this.patientRequest = {};
+			this.patientRequest.history = {};
+			this.patientRequest.history.allergies = [];
+
+			this.allergy = {};
+			//var historyRequest = this.patientRequest.history;
+
+			//var allergy = this.allergy;
 			
-			var historyRequest = {};
 
-			historyRequest.allergies = [];
-			historyRequest.vaccines = [];
-			historyRequest.habits = [];
 
-			this.allergy = [];
-			this.vaccine = [];
-			this.habit = [];
-
-			this.addAllergy = function(historyRequest){
-				historyRequest.allergies.push(this.allergy);
-				this.allergy = [];
+			this.addAllergy = function(main){
+				console.log("en addAllergy");
+				console.log(main.allergy);
+				main.patientRequest.history.allergies.push(main.allergy);
+				main.allergy = {};
 			};
 
-			this.addVaccine = function(historyRequest){
-				historyRequest.vaccines.push(this.vaccine);
-				this.vaccine = [];
-			};
-
-			this.addHabit = function(historyRequest){
-				historyRequest.habits.push(this.habit);
-				this.habit = {};
-			};
 	});
 })()
