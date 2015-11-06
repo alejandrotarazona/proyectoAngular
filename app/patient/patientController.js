9a0cd58b0312c1aa2440ed976ae38da5eb21b962
Alejandro Tarazona <alejandrotarazona@gmail.com>
2015-11-06 13:45:09 -0430

    Terminada carga y descarga de archivos.

--- app/patient/patientController.js
+++ app/patient/patientController.js
@@ -11,12 +11,6 @@
 		this.inConsult = false;
 		this.createConsultTab = 1;
 		this.otherOps = false;
-		
-
-
-		this.defaultImg = {
-			url : '/resource/images/nophotouploaded.png'
-		};
 
 		PatientRepository.patient.get({idUser:$stateParams.idPatient}).$promise.then(function(data){
 			global.patient = data;
@@ -57,12 +51,11 @@
 
 					consult.files = [];
 					ExamRepository.examRecByConsult.query({idConsult:consult.id}).$promise.then(function(data){
-						/*data.forEach(function(exam){
-							FileRepository.fileByExam.get({idExam: exam.id}).$promise.then(function(file){
-								exam.file = file;
-								consult.files.push(file);
+						data.forEach(function(exam){
+							FileRepository.fileIdByExam.get({idExam:exam.id}).$promise.then(function(resultId){
+								exam.results = resultId;
 							});
-						});*/
+						});
 						consult.examRecieved = data;
 						//console.log("Pasado los examenes recibidos");
 					});
@@ -131,7 +124,7 @@
 			global.consultRequest.requestExams = [];
 			global.consultRequest.recieveExams = [];
 			ExamRepository.examPendingByPatient.query({idPatient:$stateParams.idPatient}).$promise.then(function(list){
-				console.log(list);
+				//console.log(list);
 				global.pendingExams = list;
 			});	
 		};
@@ -147,9 +140,18 @@
 		this.saveConsult = function(consultRequest){
 			consultRequest.patient = global.patient;
 			consultRequest.doctor = global.doctor;
+			console.log("consultRequest");
 			console.log(consultRequest);
 			ConsultRepository.consult.save(consultRequest).$promise.then(function(newConsult){
 				global.consults.push(newConsult);
+
+				consultRequest.recieveExams.forEach(function(exam){
+					console.log("exam");
+					console.log(exam);
+					ExamRepository.exam.update({id:exam.id}, exam);
+				});
+
+
 				$state.transitionTo('home.patient',{},
 						{
 							inherit:true,
@@ -246,30 +248,31 @@
 		$scope.add = function(){
 			var f = document.getElementById('file').files[0],
 		    r = new FileReader(),
-		    file = {};
+		    fileRequest = {};
 
-		    file.fileName = f.name;
+		    fileRequest.fileName = f.name;
 
 			console.log("f");
 			console.log(f);
 
 			r.onloadend = function(e){
-		    	file.file = e.target.result;
-		    	file.type = f.type;
-		    	console.log("file");
-		    	console.log(file);
+		    	fileRequest.data = e.target.result.split('').map(function(c) { return c.charCodeAt(); });
+		    	fileRequest.type = f.type;
+		    	console.log("fileRequest");
+		    	console.log(fileRequest);
 
-		    	FileRepository.file.save(file).$promise.then(function(uploadedFile){
+		    	FileRepository.file.save(fileRequest).$promise.then(function(uploadedFile){
 		    		var exam = {};
 		    		exam = global.recieve;
-		    		exam.results = uploadedFile;
-		    		console.log("uploadedFile");
-		    		console.log(uploadedFile);
+		    		exam.results.id = uploadedFile.id;
+		    		
 		    		global.consultRequest.recieveExams.push(exam);
+
 		    		global.pendingExams.splice(global.pendingExams.indexOf(exam),1);
 		    		global.recieve = null;
 		    	});
 			};
+
 			r.readAsBinaryString(f);
 		}
 
@@ -310,24 +313,18 @@
 		};
 
 		this.downloadFile = function(exam){
-			console.log("exam");
-			console.log(exam);
 
-			// Tomado de: 
-			// https://gist.github.com/ncerminara/11257943
-			// Create Base64 Object
-			//var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=0,c1=0,c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
-			/*
-			var element = document.createElement('a');
-			element.setAttribute('href', 'data:'+ file.type +';'+ base + ',' + Base64.encode(file.file));
-			element.setAttribute('download', file.fileName);
-			element.style.display = 'none';
+/*
+				FileRepository.fileByExam.get({idExam:exam.id}).$promise.then(function(downloadedFile){
+					console.log("downloadedFile");
+					console.log(downloadedFile);
+					FileRepository.downloadFile.get({idFile:downloadedFile.id});
 
-			document.body.appendChild(element);
-			element.click();
-			document.body.removeChild(element);
-			*/
+				});
+*/			
+
+			
 		};
 	});
 })()
--- repository/fileRepository.js
+++ repository/fileRepository.js
@@ -1,15 +1,18 @@
 (function(){
-	hxplus.factory('FileRepository', function($resource){
-		return {
-			file  : $resource('http\://localhost\:8080/occupational/file/:id',{id:'@id'},
-			{
-				'update':{
-					method : 'PUT'
-				}
-			}),
+    hxplus.factory('FileRepository', function($resource){
+        return {
+            file: $resource('http\://localhost\:8080/occupational/file/:id',{id: '@id'},
+                {
+                    'update':{
+                        method:'PUT',
+                    }
+                }),
 
-			filesByConsult : $resource('http\://localhost\:8080/occupational/file/byconsult/:idConsult',{idConsult:'@idConsult'}),
-			fileByExam : $resource('http\://localhost\:8080/occupational/file/byexam/:idExam',{idExam:'@idExam'})
-		}
-	})
+            filesByConsult : $resource('http\://localhost\:8080/occupational/file/byconsult/:idConsult',{idConsult:'@idConsult'}),
+            fileByExam : $resource('http\://localhost\:8080/occupational/file/byexam/:idExam',{idExam:'@idExam'}),
+            fileIdByExam : $resource('http\://localhost\:8080/occupational/file/exam/:idExam',{idExam:'@idExam'}),
+            downloadFile : $resource('http\://localhost\:8080/occupational/file/getImage/:idFile',{idFile:'@idFile'}),
+
+        }
+    })
 })()
